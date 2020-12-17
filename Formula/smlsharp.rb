cecbbed971464185e8002a7099ed421cbb6577fe
Katsuhiro Ueno <katsu@riec.tohoku.ac.jp>
2020-12-17 15:21:23 +0900

    smlsharp-3.6.0

--- Formula/smlsharp.rb
+++ Formula/smlsharp.rb
@@ -4,21 +4,24 @@ class Smlsharp < Formula
   url "https://www.pllab.riec.tohoku.ac.jp/smlsharp/download/smlsharp-3.6.0.tar.gz"
   sha256 "83790d5e6b468a08f7fb221f0c2682f4243aaff063c4c43533734e4232e7720b"
   version "3.6.0"
+  license "BSD-3-Clause"
+  revision 1
 
   bottle do
     root_url "https://www.pllab.riec.tohoku.ac.jp/smlsharp/download/homebrew-bottles"
-    sha256 "44e170173c88a214825cc1d7f53ab87c377d711c97c479e4bde17e1364304947" => :catalina
+    sha256 "64af53ad3ceb244c8a94f0871b3f70780e8ce5b67c21b5ab0215a68acdef8360" => :catalina
   end
 
-  depends_on "llvm"
+  depends_on "llvm@9"
   depends_on "massivethreads"
   depends_on "gmp"
   depends_on "yajl"
   depends_on "xz" => :build
 
   def install
-    opt_llvm_bin = Formula["llvm"].opt_prefix
-    system "./configure", "--prefix=#{prefix}", "--with-llvm=#{opt_llvm_bin}"
+    opt_llvm = Formula["llvm@9"].opt_prefix
+    opt_llvm_bin = Formula["llvm@9"].opt_bin
+    system "./configure", "--prefix=#{prefix}", "--with-llvm=#{opt_llvm}"
     system "make", "stage"
     system "make", "all"
     inreplace "src/config.mk" do |s|
